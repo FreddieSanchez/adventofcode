03832ac72b988fb2f86165e238dbe8a656a28904
Freddie Sanchez <freddie.sanchez@gmail.com>
2015-12-19 09:09:31 -0700

    Day 7 - complete!

--- 2015.day7.part1.py
+++ 2015.day7.part1.py
@@ -1,14 +1,17 @@
 #/usr/bin/python
 
-import sys,re
+import sys,re,ctypes
 
 wires = {};
+gates = {};
 
-isSignal = re.compile(r'(NOT)? ?(\d+|[a-z]+) -> ([a-z]+)');
-isGate = re.compile(r'([a-z]+|\d+) (AND|OR|[LR]SHIFT) ([a-z]+|\d+) -> ([a-z]+)');
+isUnary = re.compile(r'^(NOT)? ?(\d+|[a-z]+)$');
+isBinary = re.compile(r'^([a-z]+|\d+) (AND|OR|[LR]SHIFT) ([a-z]+|\d+)$');
 isInt = re.compile(r'^\d+$');
 isWire = re.compile(r'^[a-z]+$');
 
+def DoNot(x):
+    return 65535 - value(x)
 def DoAnd(x,y):
     return int(x) & int(y);
 def DoOr(x,y):
@@ -24,69 +27,52 @@ Ops = { 'AND' : DoAnd,
         'RSHIFT' : DoRightShift,
         }
 
-def updateWires(z,signal):
-    for key in wires.keys():
-        updated = re.sub(r'^{0} '.format(z), r'{0} '.format(signal), wires[key])
-        if (wires[key] != updated):
-          wires[key] = updated; 
-          eval(wires[key] + ' -> ' + key);
-        
-        updated = re.sub(r' {0} '.format(z), r' {0} '.format(signal), wires[key])
-        if (wires[key] != updated):
-          wires[key] = updated; 
-          eval(wires[key] + ' -> ' + key);
-
-def eval(expression):
-   
-    if (isInt.match(expression)):
-        return expression
-
-    if (isWire.match(expression)):
-        if (expression in wires and isInt.match(wires[expression])):
-            return wires[expression];
-        else:
-            return expression;
-   
-    signal = isSignal.match(expression)
-    if (signal):
-        op = signal.group(1)
-        x = eval(signal.group( 2 ))
-        z = signal.group( 3 )
-        print signal.groups();
-        if (isInt.match(x)):
-            if (op): # NOT
-                x = ~x
-            wires[z] = str(x);
-            updateWires(z,str(x));
-        else:
-            if (op):
-                wires[z] = op + ' ' + x;
-            else:
-                wires[z] = x;
-        return wires[z];
-
-    gate = isGate.match(expression)
-    if (gate):
-        x = eval(gate.group( 1 ))
-        op = gate.group( 2 )
-        y = eval(gate.group( 3 ))
-        z = gate.group( 4 )
-
-        if (isInt.match(x) and isInt.match(y)):
-            wires[z] = str(Ops[op](x,y))
-            updateWires(z,wires[z]);
-        else:
-            wires[z] = gate.group(1) + ' ' + op + ' ' + gate.group(3)
-        return wires[z]
-            
+def value(x):
+    return ctypes.c_uint16(int(x)).value
 
            
+def evaluate(signal):
+
+    if (isInt.match(signal)):
+        return signal;
+
+    #Known
+    if (signal in wires):
+        return wires[signal];
+    
+    #Not Known Yet
+    unary = isUnary.match(gates[signal])
+    if (unary):
+        op = unary.group(1)
+        x = evaluate(unary.group( 2 ))
+        if (isInt.match(x)):
+            if (op): # NOT
+                x = DoNot(x);
+            wires[signal] = str(x);
+            return wires[signal];
+    binary = isBinary.match(gates[signal])
+    if (binary):
+        x = evaluate(binary.group( 1 ))
+        op = binary.group( 2 )
+        y = evaluate(binary.group( 3 ))
+        if (isInt.match(x) and isInt.match(y)):
+            wires[signal] = str(Ops[op](x,y) % 65535)
+            return wires[signal]
+    return None
            
 
 for input in sys.stdin:
     input = input.rstrip();
-    eval(input);
+    input = re.match(r'(.+) -> (\w+)',input)
+    gates[input.group(2)] = input.group(1)
+
+a = evaluate('a');
+print a
+wires = {}
+wires['b'] = a
+
+a = evaluate('a');
+print a
 
 
-print wires;
 
