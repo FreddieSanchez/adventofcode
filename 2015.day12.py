c2a2efc8e21572b9699b418efb368562fe18d986
Freddie Sanchez <freddie.sanchez@gmail.com>
2015-12-29 09:41:34 -0700

    day 12 part 2

--- 2015.day12.py
+++ 2015.day12.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python
-import sys, re
+import sys, re, json
+
+def countNums(obj,exception):
+    total = 0;
+    if isinstance(obj, dict):
+        if exception in obj.itervalues():
+            total = 0;
+        else:
+            nums = [x for x in obj.itervalues() if isinstance(x,(int, long))]
+            total = sum(nums)
+
+            for key, value in obj.iteritems():
+                total += countNums(value,exception)
+
+    elif isinstance(obj, list):
+        nums = [x for x in obj if isinstance(x,(int, long))]
+        total = sum(nums)
+        for value in obj:
+            total += countNums(value,exception)
+    return total
+
 
 
 input = [ input.rstrip() for input in sys.stdin ]
 for i in input:
     nums = re.findall(r'-?\d+',i)
     nums = [int(x) for x in nums]
-    print sum(nums)
+
+    j = json.loads(i)
+    print countNums(j,'red')
+
