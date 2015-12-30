33c68f0329f7329ea446c4067f4ebb0866f0427a
Freddie Sanchez <freddie.sanchez@gmail.com>
2015-12-30 10:55:44 -0700

    day 13 part1

---
+++ 2015.day13.py
@@ -0,0 +1,40 @@
+
+import sys,re,itertools
+
+
+def solve(peeps):
+    all = itertools.permutations(list(peeps.iterkeys()))
+
+    totals = []
+    for combo in all:
+        prev = combo[-1]
+        total = []
+        for person in combo:
+            total.append(people[prev][person])
+            total.append(people[person][prev])
+            prev = person
+        totals.append(sum(total))
+    return max(totals)
+        
+
+
+
+people = {}
+input = [i.rstrip() for i in sys.stdin]
+regex = re.compile(r'^(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+).$')
+for i in input:
+    match = regex.match(i)
+    name = match.group(1)
+    gain = match.group(2)
+    points = int(match.group(3))
+    nextTo = match.group(4)
+
+    if name not in people:
+        people[name] = {} 
+    if  nextTo not in people:
+        people[nextTo] = {}
+    people[name][nextTo] = points if (gain == 'gain') else points * -1
+
+
+print solve(people)
+
