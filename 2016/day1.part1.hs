12836a737fb847825e284d7063f2ce3989d5ba0b
Freddie <freddie.sanchez@gmail.com>
2017-01-13 16:04:30 -0700

    day1 in haskell cont

--- 2016/day1.part1.hs
+++ 2016/day1.part1.hs
@@ -1,11 +1,20 @@
-data Direction = R | L deriving(Show)
+data Turn = R | L deriving(Show)
+data CardinalDirection = N | E | S | W deriving(Show, Enum)
 
-main = do
+turn compass direction = mod ( (case direction of R -> succ ; L -> pred ) .  compass ) 4
+
+move positions (direction, distance) =
+ | 
+
+solution = do
   contents <- getContents 
   let commands = 
     [case direction of
       'R':distance -> (R, read distance :: Int)
       'L':distance -> (L, read distance :: Int)
      | direction <- (words (filter (/=',') contents)]
+  in
+  scanl move [(0,0,N)] commands 
 
-
+main = do
+  print solution
