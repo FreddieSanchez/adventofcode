1bea6d390310ddba78dbd8477470b5bd002f440f
Freddie Sanchez <freddie.sanchez@gmail.com>
2017-01-14 11:53:50 -0700

    Day 1, part1, haskell continued.

--- 2016/day1.part1.hs
+++ 2016/day1.part1.hs
@@ -1,20 +1,43 @@
-data Turn = R | L deriving(Show)
-data CardinalDirection = N | E | S | W deriving(Show, Enum)
+data Command = R Int | L Int deriving(Show)
+data CardinalDirection = N | E | S | W deriving(Show, Enum, Bounded)
+data Position = Position { x:: Int, y::Int, d::CardinalDirection } deriving(Show)
 
-turn compass direction = mod ( (case direction of R -> succ ; L -> pred ) .  compass ) 4
+type Commands = [Command]
+type Positions = [Position]
 
-move positions (direction, distance) =
- | 
+commandDistance :: Command => Int
+commandDistance (R d) = d
+commandDistance (L d) = d
 
-solution = do
-  contents <- getContents 
-  let commands = 
+face :: CardinalDirection -> Command => CardinalDirection
+face compass direction = toEnum ( mod ( ( fromEnum compass) + (case direction of R _ -> 1; L _ -> ( -1 ) ) ) 4 )
+
+move :: Positions-> Command => Positions
+move positions command  = 
+  let currentPosition = last positions
+  in 
+    case face (d currentPosition) command  of
+       N -> [currentPosition {y=(y currentPosition) + i,d= N} | i <- [1..distance]]
+       E -> [currentPosition {x=(x currentPosition) + i,d= E} | i <- [1..distance]]
+       S -> [currentPosition {y=(y currentPosition) - i,d= S} | i <- [1..distance]]
+       W -> [currentPosition {x=(x currentPosition) - i,d= W} | i <- [1..distance]]
+       where 
+       distance = commandDistance command
+        
+
+
+solution :: Position -> Commands  => Positions
+solution position commands = scanl move [(position:[])] commands
+
+parser :: String => Commands
+parser contents = do
     [case direction of
-      'R':distance -> (R, read distance :: Int)
-      'L':distance -> (L, read distance :: Int)
-     | direction <- (words (filter (/=',') contents)]
-  in
-  scanl move [(0,0,N)] commands 
+      'R':distance -> R (read distance :: Int)
+      'L':distance -> L (read distance :: Int)
+     | direction <- words (filter (/=',') contents)]
 
 main = do
-  print solution
+  contents <- getContents 
+  print (solution (Position {x=0,y=0,d=N}) ( parser contents))
+
+
