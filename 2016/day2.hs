97c880785792318af330f3874d06f677b458d112
Freddie Sanchez <freddie.sanchez@gmail.com>
2017-01-15 16:39:26 -0700

    day 2 part 2 done! in haskell!

--- 2016/day2.hs
+++ 2016/day2.hs
@@ -1,7 +1,9 @@
+import Numeric (showHex)
+
 data Command = U | R | D | L deriving(Show)
 type Commands = [Command]
 
-
+type Button = Int
 
 parser :: String -> [Commands]
 parser contents = map (\xs -> 
@@ -14,7 +16,7 @@ parser contents = map (\xs ->
 -- 1 2 3
 -- 4 5 6
 -- 7 8 9
-move :: Int -> Command -> Int
+move :: Button -> Command -> Button
 move i c = 
   case c of 
   U -> if (i `elem` [1,2,3]) then i else i - 3
@@ -22,12 +24,30 @@ move i c =
   D -> if (i `elem` [7,8,9]) then i else i + 3 
   L -> if (i `elem` [1,4,7]) then i else i - 1
 
-findNumber:: Int -> Commands -> Int
-findNumber i c = foldl move i c
+--     1
+--   2 3 4
+-- 5 6 7 8 9
+--   A B C
+--     D
+move2 :: Button -> Command -> Button
+move2 i c =
+  case c of 
+  U -> if (i `elem` [1,2,4,5,9]) then i else if (i `elem` [6,7,8,0xA,0xB,0xC]) then i - 4 else  i - 2
+  R -> if (i `elem` [1,4,9,0xC,0xD]) then i else i + 1 
+  D -> if (i `elem` [5,0xA, 0xD, 0xC, 0x9]) then i else if (i `elem` [2,3,4,6,7,8]) then i + 4 else  i + 2
+  L -> if (i `elem` [1,2,5,0xA,0xD]) then i else i - 1 
+ 
 
-solution1 :: [Commands] -> [Int]
-solution1 commands = scanl findNumber 5 commands
+toHex :: [Button] -> [String]
+toHex button = map (\x -> showHex x "") button
+
+solution1 :: [Commands] -> [Button]
+solution1 commands = tail $  scanl (foldl move) 5 commands
+
+solution2 :: [Commands] -> [Button]
+solution2 commands = tail $ scanl (foldl move2) 5 commands
 
 main = do
   contents <- getContents 
-  print $ solution1 $ parser contents
+  print $ toHex $ solution1 $ parser contents
+  print $ toHex $ solution2 $ parser contents
