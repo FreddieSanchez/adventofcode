f5779ff076409a5802fe882382b68de4352920a7
Freddie Sanchez <freddie.sanchez@gmail.com>
2017-01-16 10:05:14 -0700

    Day 3 part 2

--- 2016/day3.hs
+++ 2016/day3.hs
@@ -4,6 +4,12 @@ parser contents = [toInts (words w)  | w <-[ l |l <- lines contents]]
                   where 
                   toInts = map (\x -> read x :: Integer)
 
+parser2 :: [[Integer]] -> [[Integer]]
+parser2 [] = []
+parser2 (x:y:z:xs) = (toList (zip3 x y z)) ++ parser2 xs
+        where
+        toList l = [[x,y,z] | (x,y,z) <- l]
+
 solution1:: [[Integer]] -> [[Integer]]
 solution1 contents = filter isTriangle contents
 
@@ -11,10 +17,8 @@ isTriangle :: [Integer] -> Bool
 isTriangle sides = let sorted = sort sides in 
                    (sum $ init sorted) > (last sorted)
 
-
-solution2 contents =1
 main = do
   contents <- getContents 
   let parsed = parser contents
   print $ length $ solution1 $ parsed
-  print $ solution2 $ parsed
+  print $ length $ solution1 $ parser2 parsed 
