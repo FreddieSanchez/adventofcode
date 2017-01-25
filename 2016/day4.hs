4d94c384cc7e9c846691a514d792c059e1d4dc5f
Freddie Sanchez <freddie.sanchez@gmail.com>
2017-01-24 22:13:13 -0700

    day 4 solution 1?

--- 2016/day4.hs
+++ 2016/day4.hs
@@ -1,26 +1,46 @@
 import Data.Char
 import Data.List (sort, group, sortBy)
 
+data Room = Room { name ::String, sectorId::Integer, checksum::String} deriving (Show)
+
 data Occurance = Occurance { letter::Char, times::Int} deriving (Show, Eq)
 
 instance Ord Occurance where
   compare b1 b2   
-    |  (times b1) == (times b2) = compare (letter b1) (letter b2)
+    |  times b1 == times b2 = compare (letter b1) (letter b2)
     |  otherwise = compare (times b1) (times b2)
 
 
 compare1 :: Occurance -> Occurance -> Ordering
 compare1 b1 b2   
-  |  (times b1) == (times b2) = compare (letter b1) (letter b2)
+  |  times b1 == times b2 = compare (letter b1) (letter b2)
   |  otherwise = compare (times b2) (times b1)
 
-lineParser :: String -> [String]
-lineParser = lines
+roomParser :: String -> Room
+roomParser s = Room {name     = takeWhile (not . isDigit) s, 
+                     sectorId = read (filter isDigit s)::Integer,
+                     checksum = filter isAlpha $ dropWhile (/='[') s }
+
+
+computeChecksum :: Room -> String
+computeChecksum room = let sorted = group $ filter isAlpha $ sort (name room)
+                        in
+                        take 5 $ map letter $ sortBy compare1 $ map genOccurance sorted
+                          where 
+                          genOccurance s = Occurance {letter = head s, times = length s}
+
+                       
+                       
+                      
+isValid :: Room -> Bool
+isValid r = checksum r == computeChecksum r
+
+solution1 :: [Room] -> Integer
+solution1 rooms = foldl (\a b -> a + sectorId b) 0 $ filter isValid rooms
 
 
 main = do
   contents <- getContents 
-  let parsed  = lineParser contents in
-    print $take 5 $ map letter $ sortBy compare1 $ map (\s -> Occurance {letter = (head s), times = ( length s)}) 
-      (group $ sort $ takeWhile isLetter (head parsed))
+  let parsed  = map roomParser $ lines contents in
+     print $  solution1 parsed
  
