f40fe89b51fdbbce753026f2c42e68a8ed3b1874
Freddie Sanchez <freddie.sanchez@gmail.com>
2017-01-24 23:05:30 -0700

    day4 part 2!

--- 2016/day4.hs
+++ 2016/day4.hs
@@ -1,5 +1,5 @@
 import Data.Char
-import Data.List (sort, group, sortBy)
+import Data.List (isInfixOf, sort, group, sortBy)
 
 data Room = Room { name ::String, sectorId::Integer, checksum::String} deriving (Show)
 
@@ -29,18 +29,34 @@ computeChecksum room = let sorted = group $ filter isAlpha $ sort (name room)
                           where 
                           genOccurance s = Occurance {letter = head s, times = length s}
 
-                       
-                       
+decryptRoomName :: Room -> Room 
+decryptRoomName room =  
+    room {name = rotate (sectorId room) (name room)}
+      where                        
+       rotate 0 name = name
+       rotate n name = rotate (n-1) (map nextChar name)
+        where
+        nextChar '-' = ' '
+        nextChar ' ' = ' '
+        nextChar 'z' = 'a'
+        nextChar c   = chr (ord c + 1)
                       
-isValid :: Room -> Bool
-isValid r = checksum r == computeChecksum r
+
+validRooms :: [Room] -> [Room]
+validRooms = filter isValid 
+              where 
+              isValid r = checksum r == computeChecksum r
 
 solution1 :: [Room] -> Integer
-solution1 rooms = foldl (\a b -> a + sectorId b) 0 $ filter isValid rooms
+solution1 rooms = foldl (\a b -> a + sectorId b) 0 $ validRooms rooms
 
+solution2 :: [Room] -> [Room]
+solution2 rooms = filter (\r -> (head $ words $ name r) == "northpole") $ map decryptRoomName $ validRooms rooms 
 
 main = do
   contents <- getContents 
   let parsed  = map roomParser $ lines contents in
-     print $  solution1 parsed
+    do 
+     print $ solution1 parsed;
+     print $ solution2  parsed ;
  
