7f35880139559d9484def70583cbf0d61373b19b
Freddie Sanchez <freddie.sanchez@gmail.com>
2017-02-04 14:53:36 -0700

    re-wrote the transpose function.

--- 2016/day6.hs
+++ 2016/day6.hs
@@ -2,20 +2,20 @@ import Data.List (group, sort, sortBy)
 
 parser = lines 
 
-eachNthLetter ls n =[ [ x!!n | x <- ls]]
+transpose' :: [[a]] -> [[a]]
+transpose' ([]:_) = []
+-- get the head of each of the lists,
+-- call transpose with the tail of all of the lists.
+transpose' x = map head x : transpose' (map tail x)
 
-cycle' ls chars accum = 
-  case chars of 
-    [] -> accum;
-    (c,n):cs -> cycle' ls cs accum ++ (eachNthLetter ls n)
 
 occurances letters f = foldl1 (\a b -> if (f a b) then a else b) $ group $ sort letters
 
 mostOccurances letters = occurances letters (\a b -> length a > length b )
 leasetOccurances letters = occurances  letters (\a b -> length a < length b )
 
-solution1 ls =  map (head . mostOccurances) $ reverse $ cycle' ls ( zip (head ls) [0..]) []
-solution2 ls =  map (head . leasetOccurances) $ reverse $ cycle' ls ( zip (head ls) [0..]) []
+solution1 ls =  map (head . mostOccurances) $ transpose' ls
+solution2 ls =  map (head . leasetOccurances) $ transpose' ls
 
 
 main = do
