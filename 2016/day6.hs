8e2985a2db1ed04666b2d6d2b466d36d27880caa
Freddie Sanchez <freddie.sanchez@gmail.com>
2017-02-03 23:11:17 -0700

    day6 part 2 done!

--- 2016/day6.hs
+++ 2016/day6.hs
@@ -9,14 +9,20 @@ cycle' ls chars accum =
     [] -> accum;
     (c,n):cs -> cycle' ls cs accum ++ (eachNthLetter ls n)
 
-mostOccurances letters = foldl1 (\a b -> if length a > length b then a else b) $ group $ sort letters
+occurances letters f = foldl1 (\a b -> if (f a b) then a else b) $ group $ sort letters
+
+mostOccurances letters = occurances letters (\a b -> length a > length b )
+leasetOccurances letters = occurances  letters (\a b -> length a < length b )
 
 solution1 ls =  map (head . mostOccurances) $ reverse $ cycle' ls ( zip (head ls) [0..]) []
+solution2 ls =  map (head . leasetOccurances) $ reverse $ cycle' ls ( zip (head ls) [0..]) []
+
 
 main = do
   contents <- getContents 
   let parsed = parser contents;
   print $ solution1 parsed 
+  print $ solution2 parsed 
  
   
 
