d30d82020f15edda9d24e68e8bcb56589d2567ad
Freddie Sanchez <freddie.sanchez@gmail.com>
2017-02-05 10:58:59 -0700

    keeping them separate worked!

--- 2016/day7.hs
+++ 2016/day7.hs
@@ -16,24 +16,24 @@ isABBA str
 containsABBA str = 
   any isABBA (divvy 4 1 str)
 
-ipContainsABBA ip = not ( containsABBA (inside ip) ) && containsABBA (outside ip)
+ipContainsABBA ip = not ( any containsABBA (inside ip) ) && any containsABBA (outside ip)
 
 makeIP ip = 
   foldl buildUp IP7 { inside = [], outside = [] } $ split (startsWithOneOf "[]") ip
   where 
     buildUp ip chunk = 
      case head chunk of
-       '[' -> ip {inside = inside ip ++ tail chunk} 
-       ']' -> ip {outside = outside ip ++ tail chunk }
-       _   -> ip {outside = outside ip ++ chunk } 
+       '[' -> ip {inside = inside ip ++ [tail chunk]} 
+       ']' -> ip {outside = outside ip ++ [tail chunk] }
+       _   -> ip {outside = outside ip ++ [chunk] } 
       
 
 
 
-solution1 ips =  filter ipContainsABBA $ map makeIP ips
+solution1 ips = length $ filter ipContainsABBA $ map makeIP ips
 
 main = do
   contents <- getContents 
   let parsed = lines contents;
-  print $ map makeIP parsed
-  --print $ solution1 parsed 
+  --print $ map makeIP parsed
+  print $ solution1 parsed 
