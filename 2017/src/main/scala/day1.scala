a045867e53ddb719361ba07be0bc42f04ebb9e97
Freddie Sanchez <freddie.sanchez@gmail.com>
2017-12-26 21:33:47 -0700

    Day 1 -- but not working for part 2, planning to refactor and try again

--- 2017/src/main/scala/day1.scala
+++ 2017/src/main/scala/day1.scala
@@ -1,7 +1,5 @@
 package adventofcode2017
-
-import scala.io.Source
-
+import scala.io.Source 
 object Day1 {
 
   def compress(nums:List[Int]): List[List[Int]] = {
@@ -21,20 +19,46 @@ object Day1 {
     compress(nums).map ( xs => (xs.head, xs.length) )
   }
 
-  def captcha(nums: List[Int]): Int = {
-   val value = 
+  def captchaNext(nums: List[Int]): Int  = {
      encode(nums).foldLeft(0)( (sum, current) => if (current._2 > 1) sum + (current._1)*(current._2-1) else sum)
+  }
+     
+  def part1Captcha(nums: List[Int]): Int = {
+   val value = captchaNext(nums)
  
    if (nums.last == nums.head)
      value + nums.last
    else 
      value
 	}
-  
+
+  def splitAndZip(nums:List[Int]) : List[Int] = {
+    val split = nums.splitAt(nums.length / 2)
+    split._1.zip(split._2).flatMap( x => List(x._1, x._2))
+  }
+
+
+  def part2Captcha(nums: List[Int]): Int = {
+
+   val split = splitAndZip(nums);
+   val splitInHalf = nums.splitAt(nums.length / 2 )
+   val lastHalfFirst = splitInHalf._2 ::: splitInHalf._1
+   val split2 = splitAndZip(lastHalfFirst)
+   val value = captchaNext(split) + captchaNext(split2)
+
+   if (nums.last == nums(nums.length / 2) - 1) {
+     value + nums.last
+   }
+   else {
+     value
+   }
+	}
+
   def main(args: Array[String]): Unit = {
     val lines = Source.fromFile("./src/main/scala/1.input").getLines.toList
     val nums = lines.flatMap(x => x).map(_.asDigit)
-    println("Part One: " + captcha(nums))
+    println("Part One: " + part1Captcha(nums))
+    println("Part Two: " + part2Captcha(nums))
   }
   
 }
--- 2017/src/test/scala/day1.scala
+++ 2017/src/test/scala/day1.scala
@@ -30,18 +30,39 @@ class Day1 extends FunSuite {
   }
 
   test("captcha 1122") {
-    assert(captcha(List(1,1,2,2)) === 3)
+    assert(part1Captcha(List(1,1,2,2)) === 3)
   }
 
   test("captcha 1111") {
-    assert(captcha(List(1,1,1,1)) === 4)
+    assert(part1Captcha(List(1,1,1,1)) === 4)
   }
 
   test("captcha 1234") {
-    assert(captcha(List(1,2,3,4)) === 0)
+    assert(part1Captcha(List(1,2,3,4)) === 0)
   }
   test("captcha 91212129") {
-    assert(captcha(List(9,1,2,1,2,1,2,9)) ===  9)
+    assert(part1Captcha(List(9,1,2,1,2,1,2,9)) ===  9)
+  }
+  
+  test("splitAndZip part 2 1212") {
+    assert(splitAndZip(List(1,2,1,2)) === List(1,1,2,2))
+  }
+  test("splitAndZip part 2 1221") {
+    assert(splitAndZip(List(1,2,2,1)) === List(1,2,2,1))
+  }
+  test("captcha part 2 1212") {
+    assert(part2Captcha(List(1,2,1,2)) === 6)
+  }
+  test("captcha part 2 123425") {
+    assert(part2Captcha(List(1,2,3,4, 2, 5)) === 4)
+  }
+
+  test("captcha part 2 123123") {
+    assert(part2Captcha(List(1,2,3,1, 2, 3)) === 12)
+  }
+
+  test("captcha part 2 12131415") {
+    assert(part2Captcha(List(1,2,1,3, 2, 4,1, 5)) === 4)
   }
 
 }
