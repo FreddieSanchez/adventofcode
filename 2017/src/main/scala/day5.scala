ef65a9e90737e2ba09d0518403216908e9c79ec4
Freddie Sanchez <freddie.sanchez@gmail.com>
2018-01-10 21:03:46 -0700

    Day 5 part 2 and refactored

--- 2017/src/main/scala/day5.scala
+++ 2017/src/main/scala/day5.scala
@@ -2,36 +2,30 @@ package adventofcode2017
 
 object Day5 {
 
-
-  def jump(instructions:List[Int], index:Int):(List[Int],Int) =  {
-    val offset = instructions(index)
-    val updatedInstructions = instructions.updated(index,  offset + 1)
-    (updatedInstructions,offset)
-  }
-
-  def solvePart1(instructions:List[Int], index:Int, jumps:Int):Int = {
-    if (index >= instructions.length) jumps
+  @annotation.tailrec
+  def solve(instructions:Vector[Int], index:Int, jumps:Int, fn:Int => Int):Int = {
+    if (index < 0 || index >= instructions.length) jumps
     else  {
-      val ( updatedInstructions, nextIndex ) = jump(instructions, index)
-      solvePart1(updatedInstructions, index + nextIndex, jumps+1)
+      val offset = instructions(index)
+      val updatedInstructions = instructions.updated(index,  fn(offset))
+      solve(updatedInstructions, index + offset, jumps + 1, fn)
     }
 
 
   }
-  def part1(instructions:List[Int]):Int = {
-    solvePart1(instructions,0,0)
+  def part1(instructions:Vector[Int]):Int = {
+    solve(instructions,0,0, {x => x + 1})
   }
 
-
-  def part2(instructions:List[Int]):Int = {
-    ???
+  def part2(instructions:Vector[Int]):Int = {
+    solve(instructions,0,0, {x => if (x >=3) x - 1 else x + 1})
   }
 
 
   def main(args: Array[String]): Unit = {
-    def parseInput(lines: List[String]): List[Int]=  lines.map(_.toInt)
+    def parseInput(lines: Vector[String]): Vector[Int]=  lines.map(_.toInt)
 
-    val lines = io.Source.fromFile("./src/main/scala/5.input").getLines.toList
+    val lines = io.Source.fromFile("./src/main/scala/5.input").getLines.toVector
     val parsed = parseInput(lines)
     println("Part One: " + part1(parsed))
     println("Part Two: " + part2(parsed)) 
--- 2017/src/test/scala/day5.scala
+++ 2017/src/test/scala/day5.scala
@@ -6,22 +6,15 @@ import adventofcode2017.Day5._
 
 class Day5 extends FunSuite {
 
-  test("part1 - valid jump") {
-    assert(jump(List(0,3,0,1,-3), 0) == (List(1,3,0,1,-3),0))
-  }
-
-  test("part1 - valid jump 1") {
-    assert(jump(List(1,3,0,1,-3), 0) == (List(2,3,0,1,-3),1))
-  }
-
-  test("part1 - valid jump 2") {
-    assert(jump(List(2,3,0,1,-3), 1) == (List(2,4,0,1,-3),3))
-  }
-
   test("solve part1 test") {
 
-    val jumps =  solvePart1(List(0,3,0,1,-3),0,0)
+    val jumps =  solve(Vector(0,3,0,1,-3),0,0, {x => x + 1})
     assert(jumps == 5)
+  }
+  test("solve part2 test") {
+
+    val jumps =  solve(Vector(0,3,0,1,-3),0,0, {x => if (x >= 3) x - 1 else x + 1})
+    assert(jumps == 10)
   }
 }
 
