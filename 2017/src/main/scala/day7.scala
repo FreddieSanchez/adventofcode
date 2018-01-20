2191defb1f285cdc3c5a02d2c6d477774d9cbd4d
Freddie Sanchez <freddie.sanchez@gmail.com>
2018-01-19 19:22:15 -0700

    part2 works for example, but not for real input

--- 2017/src/main/scala/day7.scala
+++ 2017/src/main/scala/day7.scala
@@ -1,84 +1,121 @@
 package adventofcode2017
+import scala.collection.mutable.{Map => MMap}
 
 object Day7 {
 
-  type Name = String;
+  type Name = String
   type Programs = Set[Program]
 
   case class Program(name:Name, weight: Int, children:Set[Name])
 
-
+  var programs:Programs = parse(io.Source.fromFile("./src/main/scala/7.input").getLines.toList)
+  lazy val programByName:Map[Name,Program] = programs.map(program => program.name -> program).toMap
+  lazy val allChildren:Programs = programs.flatMap(_.children).map(programByName)
   
   def parse(lines: List[String]):Programs = {
     val regex = """(\w+) \((\d+)\)( -> (.*))?""".r
 
-    val programs = lines.map({
+    val parsed = lines.map({
       case regex(name, weight, _, children) => 
-        Some(Program(name, weight.toInt, Option(children).map(_.split(", ").toSet).getOrElse(Set())))
-      case _ =>  None
+        Program(name, weight.toInt, Option(children).map(_.split(", ").toSet).getOrElse(Set()))
     }).toSet
-    
-    programs.collect({ case Some(x) => x })
-  }
-  
 
-  def bottomProgram(programs: Programs):Option[Program]= {
-    val bottom = programs.filter(program => programs.forall( prog => !prog.children.contains(program.name)))
-    Option(bottom.head)
+    parsed
   }
 
-  def part1(programs: Programs):Option[Program] = {
-    bottomProgram(programs)
-  }
-  def childByName(programs:Programs, name:Name):Option[Program] = 
-    programs.find(x => x.name == name)
-
-
-  def childWeights(programs:Programs, program:Program) : List[Int] = {
-    val children = program.children.toList.map(
-      c => {
-        val child = childByName(programs, c).getOrElse(Program("Not Found", 0, Set()))
-        sumChildrenWeights(programs,child,child.weight)
-      })
-    children
+  lazy val bottomProgram:Option[Program]= {
+    val bot = programs diff allChildren
+    if (bot.size == 1) Option(bot.head) else None
   }
 
-  def sumChildrenWeights(programs:Programs, program:Program, sum:Int):Int = {
-    if (program.children.size == 0) sum
+  lazy val part1: Program = bottomProgram.get
+
+  // Un-memoized version to get the total of all the children weights, it will 
+  // recompute the weights multiple times for the same program
+  def calcChildrenWeights(program:Program):Int ={
+    def _findChildrenWeights(program:Program, accum:Int):Int = {
+      if (program.children.isEmpty) accum
+      else {
+        val weights = for {
+          childName <- program.children.toList
+          child     =  programByName(childName)
+          weight    = _findChildrenWeights(child,child.weight + accum)
+        } yield (weight)
+        weights.sum
+      }
+    }
+    val weight = _findChildrenWeights(program,0)
+    weight
+  }
+
+
+  // Map the program name to the weight of it's children
+  lazy val programToTotalWeight = programs.map(program => 
+                                               program.name -> calcChildrenWeights(program)).toMap
+
+  def childrenWeight(program:Program):Set[(Program,Int)] =
+    program.children.map(programByName)
+                    .map( program => 
+                         (program, program.weight + programToTotalWeight(program.name)))
+
+
+  def unBalancedChild(program:Program):Option[Program] = {
+    val weights = childrenWeight(program)
+    //group by them by their weight
+    val grouped = weights.groupBy(_._2)
+    if (grouped.size == 1){
+      println(grouped)
+      None
+    }
     else {
-      val total = childWeights(programs,program).sum
-      sum + total
+      println(grouped.filter({case (weight, children) => children.size == 1}))
+      //get the child that doesn't match the others.
+      val (outcast) = grouped.filter({case (weight, children) => children.size == 1})
+                             .toSeq.head._2.toSeq.head._1
+     Some(outcast)
     }
   }
 
-  def isBalanced(programs: Programs, program:Program):Boolean = {
+
+  def part2(program:Program, parent:Option[Program]):Int = {
     println(program)
-
-    val weights = childWeights(programs,program)
-    println(weights)
-    weights.groupBy(s => s).size == 1
-  }
-
-
-  def part2(root: Program) = {
-
-
-    
-
-    
-    
-    
-
-    
+    val unbalanced = unBalancedChild(program)
+    println(unbalanced)
+    unbalanced match {
+      case Some(prog) => part2(prog,Some(program))
+      case None => 
+        // Childern are balanced, parent that needs to be rebalanced
+      {
+        println(childrenWeight(parent.get).groupBy(_._2))
+        val offBy = childrenWeight(parent.get).groupBy(_._2).map( { case (k,v) => k }).reduce(_ - _)
+        println(offBy)
+        val outcast = unBalancedChild(parent.get)
+        println(outcast)
+        outcast.get.weight + offBy
+      }
+    }
 
   }
+  
+
+
+
+//  def part2(programs:Programs, root: Program) = {
+//    def loop(program:Program):(Program, List[Int]) = {
+//      val blanaced, weights = isBalanced(programs, program)
+//      if (!balanced) {
+//        val children = program.children.map(programByName))
+//        children.flatMap(loop)
+//      } else  
+//
+//    }
+//  }
 
   def main(args: Array[String]): Unit = {
-    val lines = io.Source.fromFile("./src/main/scala/7.input").getLines.toList
-    val parsed = parse(lines)
-    val root = part1(parsed)
+
+    val root = part1
     println("Part One: " + root)
-    //println("Part Two: " + part2(parsed)) 
+    println("Part Two: " + part2(root, None))
   }
   
 }
--- 2017/src/test/scala/day7.scala
+++ 2017/src/test/scala/day7.scala
@@ -18,29 +18,36 @@ class Day7 extends FunSuite {
     |jptl (61)
     |ugml (68) -> gyxo, ebii, jptl
     |gyxo (61)
-    |cntj (57)
-    """.stripMargin
+    |cntj (57)""".stripMargin
 
    val lines = line.split("\n").toList
-   lazy val parsed = parse(lines)
+   Day7.programs = parse(lines)
    
   test("root 1") {
-    val root = bottomProgram(parsed)
+    val root = bottomProgram
 
     assert(root match {
       case Some(x) => x.name == "tknk"
-      case _ => false})
+      case None => false})
   }
 
-  test("isBalanced ugml") {
-    val balance = isBalanced(parsed, Program("ugml", 68, Set("gyxo", "ebii", "jptl")))
-    
-    assert(balance)
+  test("total weights") {
+    val p = programs.find(p => p.name == "ugml").get
+    assert(calcChildrenWeights(p) == 183)
+  }
+  test("total weights 1") {
+    val p = programs.find(p => p.name == "padx").get
+    assert(calcChildrenWeights(p) == 198)
   }
 
-  test("isBalanced tknk") {
-    val balance = isBalanced(parsed, Program("tknk", 41, Set("ugml","padx", "fwft")))
-    assert(!balance)
+  test("weights 1") {
+    val p = programs.find(p => p.name == "tknk").get
+    val p1 = programs.find(p => p.name == "ugml").get
+    assert(unBalancedChild(p).get == p1)
+  }
+  test("part 2") {
+    val p = programs.find(p => p.name == "tknk").get
+    println(part2(p,None))
   }
 }
 
