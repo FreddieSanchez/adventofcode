c4fe41f2ed0df76fef5dc0fca82a0fa4548df1d4
Freddie Sanchez <freddie.sanchez@gmail.com>
2018-05-16 23:20:58 -0700

    day 9 part2

--- 2017/src/main/scala/day9.scala
+++ 2017/src/main/scala/day9.scala
@@ -2,7 +2,7 @@ package adventofcode2017
 
 object Day9 {
  
-  def part1(stream: List[Char]) = {
+  def part1(stream: List[Char]):Int = {
     def _parse(str: List[Char], currentScore: Int, totalScore:Int, inGarbage: Boolean ): Int = {
       //println("Str: %s, currentscore: %d, totalScore: %d, inGarbage: %b".format(str,currentScore,totalScore,inGarbage))
       str match  {
@@ -23,11 +23,32 @@ object Day9 {
   }
 
   
+  def part2(stream: List[Char]):List[Char] = {
+    def _parse(str: List[Char], garbageChar:List[Char], inGarbage: Boolean ): List[Char] = {
+      //println("Str: %s, garbage: %s,  inGarbage: %b".format(str,garbageChar,inGarbage))
+      str match  {
+        case ','::xs if (!inGarbage)=> _parse(xs, garbageChar ,inGarbage)
+        case '}'::xs if (!inGarbage) => _parse(xs, garbageChar, inGarbage)
+        case '{'::xs if (!inGarbage) => _parse(xs, garbageChar, inGarbage)
+        case '<'::xs if (!inGarbage) => _parse(xs, garbageChar, !inGarbage)
+        case '<'::xs if (inGarbage)  => _parse(xs, '<'::garbageChar, inGarbage)
+        case '>'::xs if (inGarbage) => _parse(xs, garbageChar, !inGarbage)
+        case '!'::_::xs if (inGarbage) => _parse(xs, garbageChar, inGarbage)
+        case x::xs if (inGarbage) => _parse(xs, x::garbageChar,inGarbage)
+        case nil => garbageChar
+       }
+    }
 
+    _parse(stream,List(),false)
+
+  }
   def main(args: Array[String]): Unit = {
     val input = io.Source.fromFile("./src/main/scala/9.input").getLines.mkString.toList
     val instructions = part1(input)
+    val nonGarbageChar = part2(input).length
     println("Part One: %d".format(instructions))
+    println(nonGarbageChar)
+    println("Part Two: %d".format(nonGarbageChar))
   }
 
 }
--- 2017/src/test/scala/day8.scala
+++ 2017/src/test/scala/day8.scala
@@ -9,47 +9,71 @@ class Day9 extends FunSuite {
 
   test("test part1 {}") {
     
-    val total = parse("{}".toList)
+    val total = part1("{}".toList)
     assert(total == 1)
   }
   test("test part1 {{}}") {
     
-    val total = parse("{{}}".toList)
+    val total = part1("{{}}".toList)
     assert(total == 3)
   }
   test("test part1 {{{}}}") {
     
-    val total = parse("{{{}}}".toList)
+    val total = part1("{{{}}}".toList)
     assert(total == 6)
   }
   test("test part1 {{},{}}") {
     
-    val total = parse("{{},{}}".toList)
+    val total = part1("{{},{}}".toList)
     assert(total == 5)
   }
 
   test("test part1 {{{},{},{{}}}}") {
     
-    val total = parse("{{{},{},{{}}}}".toList)
+    val total = part1("{{{},{},{{}}}}".toList)
     assert(total == 16)
   }
 
   test("test part1 {<a>,<a>,<a>,<a>}") {
-    val total = parse("{<a>,<a>,<a>,<a>}".toList)
+    val total = part1("{<a>,<a>,<a>,<a>}".toList)
     assert(total == 1)
   }
   test("test part1 {{<ab>},{<ab>},{<ab>},{<ab>}}") {
-    val total = parse("{{<ab>},{<ab>},{<ab>},{<ab>}}".toList)
+    val total = part1("{{<ab>},{<ab>},{<ab>},{<ab>}}".toList)
     assert(total == 9)
   }
   test("test part1 {{<!!>},{<!!>},{<!!>},{<!!>}},") {
-    val total = parse("{{<!!>},{<!!>},{<!!>},{<!!>}},".toList)
+    val total = part1("{{<!!>},{<!!>},{<!!>},{<!!>}},".toList)
     assert(total == 9)
   }
   test("test part1 {{<a!>},{<a!>},{<a!>},{<ab>}},") {
-    val total = parse("{{<a!>},{<a!>},{<a!>},{<ab>}},".toList)
+    val total = part1("{{<a!>},{<a!>},{<a!>},{<ab>}},".toList)
     assert(total == 3)
   }
 
+  test("test part2 <>") {
+    val total = part2("<>".toList).length
+    assert(total == 0)
+  }
+  test("test part2 <random characters>") {
+    val total = part2("<random characters>".toList).length
+    assert(total == 17)
+  }
+  test("test part2 <<<<>") {
+    val total = part2("<<<<>".toList).length
+    assert(total == 3)
+  }
+  test("test part2 <{!>}>") {
+    val total = part2("<{!>}>".toList).length
+    assert(total == 2)
+  }
+  test("test part2 <!!>") {
+    val total = part2("<!!>".toList).length
+    assert(total == 0)
+  }
+  test("test part2 <{o\"i!a,<{i<a>") {
+    val total = part2("<{o\"i!a,<{i<a>".toList).length
+    assert(total == 10)
+  }
 }
 
